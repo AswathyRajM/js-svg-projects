2e8210a3a7b54c283fad0deb1468dff19b1a9f4b
AswathyRajM <aswathyraj767@gmail.com>
2021-03-01 22:42:16 +0530

    Completed

--- fibinocciFlower/app.js
+++ fibinocciFlower/app.js
@@ -7,9 +7,10 @@ ctx.globalCompositeOperation = "destination-over";
 
 let number = 0;
 let scale = 10;
+let hue = Math.random() * 360;
 
 function drawFlower() {
-  let angle = number * 0.5;
+  let angle = number * 3.3;
   let radius = scale * Math.sqrt(number);
 
   // sin transfers the angle between the range of -1 to +1
@@ -18,15 +19,16 @@ function drawFlower() {
   //cos transfers the angle between the range of +1 to -1
   let positionY = radius * Math.cos(angle) + canvas.height / 2;
 
-  ctx.fillStyle = "lightgreen";
-  ctx.strokeStyle = "blue";
-  ctx.lineWidth = 2;
+  ctx.fillStyle = "hsl(" + hue + ",100%,50%)";
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = 5;
   ctx.beginPath();
-  ctx.arc(positionX, positionY, number, 0, Math.PI * 2); //360deg
+  ctx.arc(positionX, positionY, 15, 0, Math.PI * 2); //360deg
   ctx.closePath();
   ctx.fill();
   ctx.stroke();
   number++;
+  hue++;
 }
 
 function animate() {
@@ -34,7 +36,7 @@ function animate() {
   // adjust to the device refresh rate
 
   //   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  if (number > 150) return;
+  if (number > 550) return;
   drawFlower();
   requestAnimationFrame(animate);
 }
