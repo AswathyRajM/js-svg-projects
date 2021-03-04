130b10af4d049a1bbf6187ebad50d61b96c039ee
AswathyRajM <aswathyraj767@gmail.com>
2021-03-04 17:31:48 +0530

    collision detection added

--- flaapuBird/app.js
+++ flaapuBird/app.js
@@ -13,12 +13,18 @@ let gamerspeed = 2;
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   //   ctx.fillRect(10, canvas.height - 90, 50, 50);
+
+  handleParticles();
+  handleObstacles();
   bird.update();
   bird.draw();
-  handleParticles();
+  
+  handleCollisions();
+  if (handleCollisions()) return;
   requestAnimationFrame(animate);
   angle += 0.12;
   hue++;
+  frame++;
 }
 animate();
 
@@ -33,3 +39,29 @@ window.addEventListener("keyup", (e) => {
     spacePressed = false;
   }
 });
+
+const bang = new Image();
+bang.src = "bang.png";
+
+function handleCollisions() {
+  for (i = 0; i < obstaclesArray.length; i++) {
+    if (
+      bird.x < obstaclesArray[i].x + obstaclesArray[i].width &&
+      bird.x + bird.width > obstaclesArray[i].x &&
+      ((bird.y < 0 + obstaclesArray[i].top && bird.y + bird.height > 0) ||
+        (bird.y > canvas.height - obstaclesArray[i].bottom &&
+          bird.y + bird.height < canvas.height))
+    ) {
+      //collision detected
+      ctx.drawImage(bang, bird.x, bird.y, 50, 50);
+      ctx.font = "25px Georgia";
+      ctx.fillStyle = "black";
+      ctx.fillText(
+        "Game Over, Your Score is " + score,
+        160,
+        canvas.height / 2 - 10
+      );
+      return true;
+    }
+  }
+}
