9b8d33ba8fd8acbbf8d03a45179efa7b68e8806c
AswathyRajM <aswathyraj767@gmail.com>
2021-03-08 17:34:40 +0530

    vertical animation

--- animatedButtons/app.js
+++ animatedButtons/app.js
@@ -4,3 +4,22 @@ let xFrequency = 0.00001;
 turbulence.setAttribute("baseFrequency", xFrequency + " 0.00001");
 const steps = 30;
 const interval = 10;
+
+buttons.forEach((button) => {
+  button.addEventListener("mouseover", () => {
+    xFrequency = 0.00001;
+    for (let i = 0; i < steps; i++) {
+      setTimeout(() => {
+        xFrequency += 0.009;
+        turbulence.setAttribute("baseFrequency", xFrequency + " 0.00001");
+      }, i * interval);
+
+      //to set the button state to inital after animation
+      setTimeout(() => {
+        xFrequency = 0.00001;
+        turbulence.setAttribute("baseFrequency", xFrequency + " 0.00001");
+      }, steps * interval);
+    }
+  });
+});
+
